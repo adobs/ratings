6ca27061549b38264bfdf5a5687870c0698c73f8
Hackbright Student <info@hackbrightacademy.com>
2015-10-20 12:28:19 -0700

    seed_user_data working

--- seed2.py
+++ seed2.py
@@ -1,8 +1,14 @@
-from model import db, User, Movie, Rating
+from model import db, User, Movie, Rating, connect_to_db
+from server import app
+
+# connect_to_db(app)
 
 def seed_user_data(filename):
+
+    #empty Users table before adding seed data
+
+
     log_file = open(filename)
-    users = []
 
     for line in log_file:
         data = line.strip().split("|") #data is a list
@@ -16,3 +22,9 @@ def seed_user_data(filename):
 
     db.session.commit()
 
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    db.session.query(User).delete()
+    db.session.commit()
+    seed_user_data("seed_data/u.user")
