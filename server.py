5d663750803d17e97771ce7f9a00036ceee5eb64
Hackbright Student <info@hackbrightacademy.com>
2015-10-22 16:58:52 -0700

    working on rating movies

--- server.py
+++ server.py
@@ -22,6 +22,10 @@ def index():
 
     return render_template("home.html")
 
+
+################ MOVIE AND USER LISTS/INFO ROUTES ################
+
+
 @app.route("/users")
 def user_list():
     """Show list of users"""
@@ -34,31 +38,41 @@ def user_list():
 def show_user_info(user_id):
     """Show info for a particular user"""
 
+    #get the user object out of the database and pull out all the info
+    #it turns out we didn't need to pull out (d'oh!)
     user = db.session.query(User).filter(User.user_id == user_id).one()
     fname = user.fname
     lname = user.lname
     email = user.email
     age = user.age
     zipcode = user.zipcode
+
+    #get all of the user's previous ratings
     ratings = (db.session.query(Movie.title, Rating.score)
                          .join(Rating)
                          .filter(Rating.user_id == user_id)
                          .all())
 
+    #display user info page
     return render_template("user-info.html", user_id=user_id, fname=fname,
                            lname=lname, email=email, age=age, zipcode=zipcode, 
                            ratings=ratings)
+
 
 @app.route("/movies")
 def movie_list():
     """Show list of movies"""
 
+    #get movie titles and id's out of the database
     movies = (db.session.query(Movie.title, Movie.movie_id)
                         .order_by(Movie.title)
                         .all())
 
+    #deal with encoding (accented characters are the issue here)
     for title, movie_id in movies:
         title = title.encode("latin-1")
+
+    #display movie list page
     return render_template("movies.html", movies=movies)
 
 
@@ -66,9 +80,14 @@ def movie_list():
 def show_movie_info(movie_id):
     """Show info for a particular movie"""
 
+    #retrieve the movie object given the movie id
     movie = db.session.query(Movie).filter(Movie.movie_id == movie_id).one()
+
+    #calculate average rating
     avg_rating = (db.session.query(db.func.avg(Rating.score))
                            .filter(Rating.movie_id == movie_id).one())[0]
+
+    #get user's previous rating, if any
     users_rating = (db.session.query(Rating.score)
                               .filter(Rating.user_id == 
                                       session.get("user_id", None))
@@ -76,16 +95,67 @@ def show_movie_info(movie_id):
     if users_rating:
         users_rating = users_rating[0]
 
+    #get a list of all ratings for this movie
     ratings = (db.session.query(User.user_id, Rating.score)
                          .join(Rating)
                          .filter(Rating.movie_id == movie_id)
                          .order_by(User.user_id)
                          .all())
 
+    #display the movie info page
     return render_template("movie-info.html", movie=movie,
                                               users_rating=users_rating, 
                                               avg_rating=avg_rating, 
                                               ratings=ratings)
+
+
+############################ RATING ROUTES ############################
+
+
+@app.route("/update-rating/<int:movie_id>", methods=["POST"])
+def update_rating(movie_id):
+    """Update logged-in user's rating of given movie"""
+
+    #get the rating from the form
+    score = request.form.get("rating")
+
+    #retrieve the old rating record from the database
+    old_rating_object = (db.session.query(Rating)
+                                   .filter(Rating.user_id == 
+                                           session["user_id"],
+                                           Rating.movie_id == movie_id)
+                                   .one())
+
+    #update the rating
+    old_rating_object.score = score
+
+    #push the change to the database
+    db.session.commit()
+
+    #stay on the movie info page
+    return redirect("/movie/" + str(movie_id))
+
+
+@app.route("/rate/<int:movie_id>", methods=["POST"])
+def rate_movie(movie_id):
+    """Add logged-in user's rating for given movie"""
+    
+    #get the rating from the form
+    score = request.form.get("rating")
+
+    #create a new record and add it to the queue
+    new_rating = Rating(movie_id=movie_id, user_id=session["user_id"], 
+                        score=score)
+    db.session.add(new_rating)
+
+    #commit changes
+    db.session.commit()
+
+    #reload movie page including new rating
+    return redirect("/movie/" + str(movie_id))
+
+
+################ LOGIN, LOGOUT, AND REGISTRATION ROUTES ################
 
 
 @app.route("/registration")
@@ -185,6 +255,7 @@ def log_out():
         session.clear()
         flash(message.format(name=fname))
 
+    #go back to the homepage
     return redirect("/")
 
 
